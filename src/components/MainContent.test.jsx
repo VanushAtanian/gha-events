4b4176851db485ece25ac44cb3004dd2c742254a
Atanyan, Vanush <VANUSH_ATANYAN@homedepot.com>
2024-01-19 04:38:32 +0400

    Try not to skip

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -4,6 +4,7 @@ import userEvent from '@testing-library/user-event';
 
 import MainContent from './MainContent';
 // Comment
+// Yet anothoer comment
 describe('MainContent', () => {
   it('should render a button', () => {
     render(<MainContent />);
